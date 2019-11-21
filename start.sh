151c604fe5a9e8adccf974ea6cb5c59c1155abf8
Pettersson, Simon <Simon.Pettersson@flir.com>
2019-11-21 15:27:53 +0100

    blah

--- start.sh
+++ start.sh
@@ -2,10 +2,6 @@
 set -e
 
 INPUT_BRANCH=${INPUT_BRANCH:-master}
-INPUT_FORCE=${INPUT_FORCE:-false}
-INPUT_DIRECTORY=${INPUT_DIRECTORY:-'.'}
-_FORCE_OPTION=''
-REPOSITORY=${INPUT_REPOSITORY:-$GITHUB_REPOSITORY}
 
 echo "Push to branch $INPUT_BRANCH";
 [ -z "${INPUT_GITHUB_TOKEN}" ] && {
@@ -13,12 +9,8 @@ echo "Push to branch $INPUT_BRANCH";
     exit 1;
 };
 
-if ${INPUT_FORCE}; then
-    _FORCE_OPTION='--force'
-fi
+remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
-cd ${INPUT_DIRECTORY}
+git checkout -B ${INPUT_BRANCH}
+git push "${remote_repo} ${INPUT_BRANCH}
 
-remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${REPOSITORY}.git"
-
-git push "${remote_repo}" HEAD:${INPUT_BRANCH} --follow-tags $_FORCE_OPTION;
